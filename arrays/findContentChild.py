299b5c52077b70beb97ad6a01fddbd88cf206a6b
k.hisomov <normok_9595@bk.ru>
2022-10-05 18:37:24 +0300

    findcontentChildre

---
+++ arrays/findContentChild.py
@@ -0,0 +1,17 @@
+def findContentChildren(g: list[int], s: list[int]) -> int:
+
+
+    count = 0
+    i = 0
+    j = 0
+    
+    while i < min(len(g), len(s)):
+        if g[i] <= s[j]:
+            count += 1
+            i += 1
+        j += 1
+
+    return count
+
+
+print(findContentChildren(g=[1, 2, 3, 4], s=[1, 2, 3, 4, 5, 6, 7]))
