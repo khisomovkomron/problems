b78e8819af4466b3c2a6ed71d2062125ec5e3bc3
k.hisomov <normok_9595@bk.ru>
2022-11-24 23:55:11 +0300

    decorator property

--- learning/dunders/decorators.py
+++ learning/dunders/decorators.py
@@ -1,14 +1,50 @@
 
 class Decorators:
     
-    @property
-    def property_decorator(self):
-        pass
-
-    @classmethod
-    def classmethod_decorator(cls):
-        pass
+    def __init__(self, new, old, other):
+        self.__new = new # private instance of attribute value
+        self.__old = old
+        self.__other = other
     
-    @staticmethod
-    def staticmethod_decorator():
-        pass
+    # property decorators add new method to Class,
+    # you can call class new method by Class().new
+    @property
+    def new(self):
+        return self.__new
+    
+    # setter is used when value of method instance is changed to new value
+    @new.setter
+    def new(self, value):
+        self.__new = value
+    
+    @property
+    def old(self):
+        return self.__old
+
+    @old.setter
+    def old(self, value):
+        self.__old = value
+  
+    @property
+    def others(self):
+        return self.__other
+    
+    # deleter deletes created method from a class
+    @others.deleter
+    def others(self):
+        print('Deleting')
+        del self.__other
+
+
+decorators = Decorators('new', 'old', 'others')
+print(decorators.old)
+print(decorators.new)
+print(decorators.others)
+
+decorators.old = "OLD1"
+decorators.new = "NEW1"
+print(decorators.old)
+print(decorators.new)
+
+del decorators.others
+print(decorators.others)
