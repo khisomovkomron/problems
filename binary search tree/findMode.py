bea708bfa6353314d36564ae8ca951c3d1f09fdf
Komron Khisomov <normok_9595@bk.ru>
2023-01-13 18:42:10 +0500

    complete findMode

--- binary search tree/findMode.py
+++ binary search tree/findMode.py
@@ -10,10 +10,25 @@ class TreeNode:
 
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
 
-        if root is None:
-            return []
-        ls = []
+        if root.val == 0:
+            return [0]
 
-        if root.left == root:
-            ls.append(root.left)
-            
+        nums, stack = {}, []
+
+        while True:
+            while root:
+                if root.val in nums.keys():
+                    nums[root.val] += 1
+                else:
+                    nums[root.val] = 1
+
+                stack.append(root) 
+                root = root.left
+
+            if not stack:
+                break
+            root = stack.pop().right
+
+        maxValue = max(nums.values())
+
+        return [k for k, v in nums.items() if maxValue == v]
