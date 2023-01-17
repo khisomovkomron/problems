34662d249db7cd9d438d04107fab2fc5ee099534
Komron Khisomov <normok_9595@bk.ru>
2023-01-17 11:34:24 +0500

    increasingBST

---
+++ binary search tree/increasingBST.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+
+class TreeNode:
+
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+
+    def increasingBST(self, root: Optional[TreeNode]) -> TreeNode:
+
+        output = []
+        self.inorder(root, output)
+
+        tree = TreeNode(val=output[0])
+        tmp = tree
+
+        for i in output[1:]:
+            tmp.right = TreeNode(val=i)
+            tmp = tmp.right
+        return tree
+
+    def inorder(self, root, output):
+        if root is None:
+            return
+
+        self.inorder(root.left, output)
+        output.append(root.val)
+        self.inorder(root.right, output)
